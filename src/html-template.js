c49fe7483a40424e685dc2fe8f6dc345ecef8112
MarilynPapadopoulos <marilyn.volk@gmail.com>
2021-04-29 18:31:48 -0400

    added Bootstrap

--- src/html-template.js
+++ src/html-template.js
@@ -25,7 +25,7 @@ console.log("hello");
                 `
             }
             if (teamArray[i].role === 'Engineer') {
-                //detail = "GitHub: " + teamArray[i].github;
+               
                 detail = detail + `
                 <div class="card-body">
                     <h5 class="card-title">${name}</h5>
@@ -39,7 +39,7 @@ console.log("hello");
                 `
             }
             if (teamArray[i].role === 'Intern') {
-                //detail = "School: " + teamArray[i].school;
+                
                 detail = detail + `
                 <div class="card-body">
                     <h5 class="card-title">${name}</h5>
@@ -53,7 +53,7 @@ console.log("hello");
                 `
             }
     }
-
+console.log(detail);
 
         return `
         <!DOCTYPE html> 
@@ -62,20 +62,24 @@ console.log("hello");
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
             <meta http-equiv="X-UA-Compatible" content="ie=edge">
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.min.js" integrity="sha512-XKa9Hemdy1Ui3KSGgJdgMyYlUg1gM+QhL6cnlyTe2qzMCYm4nAZ1PsVerQzTTXzonUR+dmswHqgJPuwCq1MaAg==" crossorigin="anonymous"></script>
-            <link rel="stylesheet" href="styles.css">
+            
             <title>Team Profile Generator</title>
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+            <link rel="./dist/stylesheet" href="styles.css">
         </head>
         
         <body>
-            <h1>My Team</h>
+            <h1>My Team</h1>
         
             <div class="card" style="width: 18rem;">
                 ${detail}
             </div>
         
-        
-        
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous"></script>
+          
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
         </body>
         </html>
         `;    
